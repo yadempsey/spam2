627be7a6629fabac88c16e066863f917cebf6f97
yadempsey <58429088+yadempsey@users.noreply.github.com>
2019-12-02 14:20:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = [''];
+const myid = ['405683225445531648'];
 const prefix = ['-'];
 
 client.on('ready', () => {
